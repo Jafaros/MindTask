22b891c1190aa55a825f4f535d598cfb3609bcd5
Jafaros <Grapetr@seznam.cz>
2025-11-11 23:22:09 +0100

    fontawesome library + example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	import { faCode } from '@fortawesome/free-solid-svg-icons';
+	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+</script>
+
+<h1><FontAwesomeIcon icon={faCode} class="text-white" /> Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
