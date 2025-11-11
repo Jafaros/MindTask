30132ba641824d66c4ba37753e14bfce4ae2f1dc
Jafaros <Grapetr@seznam.cz>
2025-11-11 20:08:04 +0100

    Project init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'com.petrgrajciar.mindtask',
+	appName: 'MindTask',
+	webDir: 'build'
+};
+
+export default config;
